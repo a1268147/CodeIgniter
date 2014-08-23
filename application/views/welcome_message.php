6124d14db101fd6b7aa889f954edc9b7f11a9897
a1268147 <a1268147@trbvm.com>
2014-08-23 14:39:36 -0300

    Lets do more one test

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -106,6 +106,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<code>application/controllers/Welcome.php</code>
 
 		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+
+		<p>It's just a test to try learn about CodeIgniter and GitHub</p>
+
 	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
