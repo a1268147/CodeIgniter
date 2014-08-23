5683d5fe144656985f49dcabfb35ebf8c7769e7c
a1268147 <a1268147@trbvm.com>
2014-08-23 12:25:09 -0300

    Testing GitHub and CodeIgniter

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -29,7 +29,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter - Test</title>
+	<title>Welcome to CodeIgniter</title>
 
 	<style type="text/css">
 
